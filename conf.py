f4b422b51a460f5627cfcaab0d7034770215204c
lidashuang <ldshuang@gmail.com>
2012-10-25 11:47:33 -0400

    fix

--- conf.py
+++ conf.py
@@ -91,8 +91,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = 'default'
-html_theme = 'nature'
+html_theme = 'default'
+# html_theme = 'nature'
 #html_theme = os.environ.get("SPHINX_THEME") or 'greencloud'
 
 # Theme options are theme-specific and customize the look and feel of a theme
